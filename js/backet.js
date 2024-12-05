83d099344dce9225efcde5eb01815c1922d94521
mutallapova11 <olgamutallapova2@gmail.com>
2024-12-05 04:37:27 -0800

    change backet.js

--- js/backet.js
+++ js/backet.js
@@ -1,47 +1,41 @@
 
-
-
-
-
-
-
-let basket_json = {
-    1: {
+let basket_json = [
+    {
         'img': '../media/chocolate/01.png',
         'name': 'Zolotoy',
         'price': 15000,
         'count': 3,
         'total_price': 45000
     },
-    2: {
+     {
         'img': '../media/chocolate/04.png',
         'name': 'Kit Kat',
         'price': 12000,
         'count': 1,
         'total_price': 12000
     },
-    3: {
+     {
         'img': '../media/drink/03.png',
         'name': 'coca-cola',
         'price': 14000,
         'count': 3,
         'total_price': 42000
     },
-    4: {
+     {
         'img': '../media/chocolate/13.png',
         'name': 'Alpen Gold',
         'price': 13000,
         'count': 3,
         'total_price': 39000
     },
-    5: {
+    {
         'img': '../media/chocolate/12.png',
         'name': 'Alpen Gold',
         'price': 13000,
         'count': 3,
         'total_price': 39000
     }
-};
+];
 
 let backet = document.querySelector('.backet');
 
